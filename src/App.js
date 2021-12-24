f500de516e2e69ba7affa75b8c03f9f37db64239
Chandrashekhar Koli <btechfinal2022@gmail.com>
2021-12-24 22:14:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Chandrashekahr
+          Learn React with me, Chandrashekahr
         </a>
       </header>
     </div>
